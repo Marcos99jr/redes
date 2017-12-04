fe74b24e2e09fd89a4f413e203a45f2166d3c5de
luisantosc <luisantosc2@gmail.com>
2017-12-03 23:01:46 -0200

    m1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<form action="redes.php" method="post">
+			<label for="texto"/> Texto Claro
+			<input type="text" name="txtc"/><br>
+			<label for="senha"/> Senha
+			<input type="text" name="senha"/><br>
+			<input type="submit" value="Criptografar"/>
+		</form>
+	</body>
+</html>
